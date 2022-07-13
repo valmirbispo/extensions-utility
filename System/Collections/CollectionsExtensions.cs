7e57c4e3d30ade1546bf0114c5bd6ab9d5c05f91
Valmir F Bispo <valmir.bispo@adm.unip.br>
2022-07-13 15:23:10 -0300

    First method for collections

---
+++ System/Collections/CollectionsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vfb_extensions_utility.System.Collections
+{
+    public static class CollectionsExtensions
+    {
+        public static bool IsEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || !enumerable.Any();
+        }
+    }
+}
