8f800752b7e7579505a3ae952ff58f67b9c30df7
Valmir F Bispo <valmir.bispo@adm.unip.br>
2022-07-18 12:17:59 -0300

    rename method

--- System/Collections/CollectionsExtensions.cs
+++ System/Collections/CollectionsExtensions.cs
@@ -2,7 +2,13 @@
 {
     public static class CollectionsExtensions
     {
+        [Obsolete("Will be replaced by IsNullOrEmpty()")]
         public static bool IsEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || !enumerable.Any();
+        }
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
         {
             return enumerable == null || !enumerable.Any();
         }
